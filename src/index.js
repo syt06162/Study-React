ce026488f1434d5564116b2408aa9035fac0ff48
sherlock_notebook <syt06162@naver.com>
2022-08-17 02:15:46 +0900

    mini-blog-project all

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import Blocks from './chapter_15/Blocks';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blocks/>
+    <App/>
   </React.StrictMode>,
   document.getElementById("root")
 );
